0cf336cf61db15bffa2b7e74c81882f793a03bb7
bakir <bsabljica1@etf.unsa.ba>
2018-12-24 10:49:17 +0100

    first commit

---
+++ src/ba/unsa/etf/rpr/Drzava.java
@@ -0,0 +1,42 @@
+package ba.unsa.etf.rpr;
+
+public class Drzava {
+    private int id;
+    private String naziv;
+    Grad glavniGrad;
+
+    public Drzava (int id, String naziv, Grad glavniGrad) {
+        this.id=id;
+        this.glavniGrad=glavniGrad;
+        this.naziv=naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setGlavniGrad(Grad glavniGrad) {
+        this.glavniGrad = glavniGrad;
+    }
+
+    public Grad getGlavniGrad() {
+        return glavniGrad;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
